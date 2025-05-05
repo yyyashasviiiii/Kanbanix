fcd9ea780de8be3bb4e415c86ebcaeea7b3b5258
Yashasvi Shukla <yashasvigot@gmail.com>
2025-05-05 11:13:49 +0530

    edit delete task feature added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let draggedCard = null;
+let rightClickedCard = null;
 function addTask(columnId) {
     const input = document.getElementById(`${columnId}-input`);
     const taskText = input.value.trim();
@@ -21,25 +22,57 @@ function createTaskElement(taskText) {
     element.draggable = true;
     element.addEventListener("dragstart", dragStart);
     element.addEventListener("dragend", dragEnd);
+    element.addEventListener("contextmenu", function(event) {
+        event.preventDefault();
+        rightClickedCard = this;
+        showContextMenu(event.pageX, event.pageY);
+        
+    })
     return element;
 }
 
 function dragStart() {
     this.classList.add("dragging");
     draggedCard = this;
-    console.log(this)
 }
 
 function dragEnd() {
     this.classList.remove("dragging");
 }
 
-const columns = document.querySelectorAll(".columns .tasks");
+const columns = document.querySelectorAll(".tasks");
 columns.forEach((column) => {
     column.addEventListener("dragover", dragOver);
+    // column.addEventListener("drop", drop);
 })
 
 function dragOver(event) {
     event.preventDefault();
     this.appendChild(draggedCard);
+}
+
+const contextmenu = document.querySelector(".context-menu");
+function showContextMenu(x, y) {
+    contextmenu.style.left = `${x}px`;
+    contextmenu.style.top = `${y}px`;
+    contextmenu.style.display = "block";
+}
+
+document.addEventListener("click", () => {
+    contextmenu.style.display = "none";
+})
+
+function editTask() {
+    if(rightClickedCard !== null) {
+        const newTaskText = prompt("Edit task - ",rightClickedCard.textContent);
+        if(newTaskText !== "") {
+            rightClickedCard.textContent = newTaskText;
+        }
+    }
+}
+
+function deleteTask() {
+    if (rightClickedCard !== null) {
+        rightClickedCard.remove();
+    }
 }
